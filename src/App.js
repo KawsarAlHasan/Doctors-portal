5be3f37c7c30d3f937882ace6b07e76799d2d84f
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-06-05 16:48:58 +0600

    added Navber

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Navber from './Pages/Shared/Navber';
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
-      <button class="btn w-64 rounded-full">Button</button>
+      <Navber></Navber>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
